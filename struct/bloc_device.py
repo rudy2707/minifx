ebe0378c987fcfe9e14ff02bbf07519f1a3aff48
Hohn Rudolf <rudolf.hohn@gmail.com>
2015-05-08 10:55:21 +0200

    adding files to git

---
+++ struct/bloc_device.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# emulate a simple bloc device using a file
+# reading it only by bloc units
+from constantes import *
+
+class bloc_device(object):
+    def __init__(self,blksize,pathname):
+        return
+    def read_bloc(self,bloc_num,numofblk=1):
+        return
+    def write_bloc(self,bloc_num,bloc):
+        return
---
+++ struct/minix_superbloc.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from constantes import *
+
+class minix_superbloc(object):
+    def __init__(self,bloc_device):
+        return
