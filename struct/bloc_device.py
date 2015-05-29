53f3e2306b8e7ce673c3f49373649845d8571897
rafael_0792 <martins.gomes.rafael@gmail.com>
2015-05-29 11:16:00 +0200

    Class bloc_device filled.

--- struct/bloc_device.py
+++ struct/bloc_device.py
@@ -5,10 +5,15 @@ from constantes import *
 
 class bloc_device(object):
     def __init__(self,blksize,pathname):
-    	self.blksize = blksize
-    	self.pathname = pathname
+        self.blksize = blksize
+        self.pathname = pathname
+        self.blocfile = os.open(self.pathname, os.O_RDWR)
         return
     def read_bloc(self,bloc_num,numofblk=1):
-        return
+        #os.SEEK_SET permet de parcourir le bloc depuis le debut
+        os.lseek(self.pathname, bloc_num*BLOCK_SIZE, os.SEEK_SET)
+        return os.read(self.blocfile, bloc_num)
     def write_bloc(self,bloc_num,bloc):
-        return
+        #os.SEEK_SET permet de parcourir le bloc depuis le debut
+        os.lseek(self.pathname, bloc_num*BLOCK_SIZE, os.SEEK_SET)
+        return os.write(self.blocfile, bloc_num)
