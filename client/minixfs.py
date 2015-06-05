8c1052993a9e09500d2ed44036960ce8744782e5
Hohn Rudolf <rudolf.hohn@gmail.com>
2015-06-05 15:45:47 +0200

    Test 9 done !

--- client/minixfs.py
+++ client/minixfs.py
@@ -37,29 +37,57 @@ class minix_file_system(object):
     #Inode 0 is never and is always set.
     #according to the inodes bitmap
     def ialloc(self):
-        return
+        i = 0
+        while self.inode_map[i]:
+            i += 1
+        self.inode_map[i] = 1
+        return i
 
     #toggle an inode as available for the next ialloc() 
     def ifree(self,inodnum):
-        return 
+        self.inode_map[inodnum] = 0
+        return
+
     #return the first free bloc index in the volume. The bitmap
     #indicate the index from the bloc zone, add first_datazone then
     #to the bloc index
     def balloc(self):
-        return
+        i = 0
+        while self.zone_map[i]:
+            i += 1
+        self.zone_map[i] = 1
+        return i+self.superBlock.s_firstdatazone
     
     #toggle a bloc as available for the next balloc() 
     #blocnum is an index in the zone_map
     def bfree(self,blocnum):
+        self.zone_map[blocnum] = 0
         return
     
     def bmap(self,inode,blk):
-        #if (blk < inode[14:15])
+        if blk < 7:
+            return inode.i_zone[blk]
+        blk -= 7
+        if blk < 512:
+            indirect_bloc = self.imgMinixFs.read_bloc(inode.i_indir_zone)
+            return struct.unpack_from("<H", indirect_bloc, blk*(BLOCK_SIZE/512))[0]
+        blk -= 512
+        if  blk < 512*512:
+            indirect_bloc = self.imgMinixFs.read_bloc(inode.i_dbl_indr_zone)
+            indirect_bloc2 = self.imgMinixFs.read_bloc(struct.unpack_from("<H", indirect_bloc, blk*(BLOCK_SIZE/(512*512)))[0])
+            return struct.unpack_from("<H", indirect_bloc2, (blk % 512) * (BLOCK_SIZE/(512)))[0]
 
         return
     
     #lookup for a name in a directory, and return its inode number, given inode directory dinode
     def lookup_entry(self,dinode,name):
+        for i in range(0,dinode.i_size):
+            blk = self.imgMinixFs.read_bloc(self.bmap(dinode, i))
+            for j in range(0, BLOCK_SIZE/INODE_SIZE):
+                inode = blk[INODE_SIZE*j:INODE_SIZE*(j+1)]
+                if name in inode[2:14]:
+                #if inode[2:14] == name:
+                    return struct.unpack("<H", inode[0:2])[0]
         return
     
     #find an inode number according to its path
